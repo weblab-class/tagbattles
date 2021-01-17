083b928c7f10a290ec90547cc22288ff012ea620
Vedantha <v_vedantha@outlook.com>
2021-01-16 21:59:50 -0800

    Added player logic for switching screens

--- client/src/components/pages/Player.js
+++ client/src/components/pages/Player.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "@reach/router";
 import "./Player.css";
-import PromptCardSelector from "../modules/PromptCardSelector.js";
-//import socketIOClient from "select.io-client";
-import WinnerSelector from "../modules/WinnerSelector.js";
-import { get } from "../../utilities";
+import ResponseSelector from "../modules/ResponseSelector.js";
+import DisplayCard from "../modules/DisplayCard.js";
+import JudgingReplica from "../modules/JudgingReplica.js";
+import { socket } from "../../client-socket.js";
+import { get, post } from "../../utilities";
 /*
-
-State: Selecting prompt cards, Waiting, Selecting Winner
-
-Remove in a moment    
+  prompt card at the top (should be loading until it is complete)
+  gonna have your response cards
+  when you click on one it sends that information over
 */
-
-
-
 /**
  * Description of Player
  *
@@ -28,22 +25,34 @@ class Player extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentState : 'promptSelector'
+      currentState: 'choosingResponse',
     }
   }
   
   componentDidMount() {
-
+    socket.on("thinkingPlayers", (data) => {
+      if (data.thinkingPlayers === 0) {
+        this.setState({
+          currrentState: 'judging'
+        })
+      }
+    });
   }
 
   // Figuring out the black card code
+  selectedResponse = () => {
+    this.setState({
+      currentState : 'judging'    
+    })
+  }
 
   // Selecting the 
   render() { 
     return (
       <>
-        <PromptCardSelector gameID={this.props.gameID} />
-        <WinnerSelector gameID={this.props.gameID} />
+        {this.state.card ? <DisplayCard text = {this.state.card}/> : <p>Loading</p>}
+        {this.state.currentState === 'choosingResponse' ? <ResponseSelector submitResponse= {this.selectedResponse}/> :
+        <JudgingReplica />}
       </>
     );
   }
