a56d605267107119a238dad85c9533c4a4517bc8
Vedantha <v_vedantha@outlook.com>
2021-01-16 21:57:49 -0800

    Updated navbar

--- client/src/components/modules/Navbar.js
+++ client/src/components/modules/Navbar.js
@@ -6,34 +6,37 @@ import "./Navbar.css";
 *
 */
 class Navbar extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props){
+      super(props);
+  }
 
-    render(){
-        return (
-            <nav>
-                {this.props.userId ? (
-                <GoogleLogout
-                    clientId={this.props.GOOGLE_CLIENT_ID}
-                    buttonText="Logout"
-                    onLogoutSuccess={this.props.handleLogout}
-                    onFailure={(err) => console.log(err)}
-                />
-                ) : (
-                <GoogleLogin
-                    clientId={this.props.GOOGLE_CLIENT_ID}
-                    buttonText="Login"
-                    onSuccess={this.props.handleLogin}
-                    onFailure={(err) => console.log(err)}
-                />
-                )}
-                <div>
-                    
-                </div>
-            </nav>
-        )
-    }
+  render(){
+    return (
+      <nav>
+        {this.props.userId ? (
+          <GoogleLogout
+            clientId={this.props.GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+    	      onLogoutSuccess={this.props.handleLogout}
+            onFailure={(err) => console.log(err)}
+          />
+          ) : (
+          <GoogleLogin
+            clientId={this.props.GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.props.handleLogin}
+            onFailure={(err) => console.log(err)}
+          />
+        )}
+        <div>
+					<ul className = "nav-list">
+						<li><a>New Game</a></li>
+						<li><a>Join Game</a></li>
+					</ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Navbar;
