62915a8f65d010f0aa522794e26ce2aa0590677c
Kevin Tong <kevintong@C02CJ5N6MD6M.tld>
2021-01-24 21:34:49 -0800

    Added dropdown navbar menu for mobile devices

--- client/src/components/modules/Navbar.js
+++ client/src/components/modules/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "@reach/router";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 import HamburgerIcon from './icons/hamburger.js';
 import Logo from './icons/logo.js';
+import CloseButton from './icons/close.js';
 import "./Navbar.css";
 
 const GOOGLE_CLIENT_ID = "640440795885-4do41cm5va1aumbs67c398b1m8m2574o.apps.googleusercontent.com";
@@ -15,10 +16,17 @@ const GOOGLE_CLIENT_ID = "640440795885-4do41cm5va1aumbs67c398b1m8m2574o.apps.goo
 class Navbar extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      listClosed: true,
+    }
   }
 
   openNav = () => {
-
+    this.setState((prevState) => {
+      return {
+        listClosed: !prevState.listClosed,
+      }
+    })
   }
 
   render(){
@@ -30,15 +38,19 @@ class Navbar extends Component {
           </Link>
         </div>
         <div className = "NavBar-hamburger">
-          <HamburgerIcon func = {this.openNav}/>
+          {this.state.listClosed ?
+            <HamburgerIcon func = {this.openNav}/>
+          :
+            <CloseButton func = {this.openNav} location = "nav"/>
+          }
         </div>
-        <div className = "NavBar-nav-links">
+        <div className = {this.state.listClosed ? "NavBar-nav-links-closed" : "NavBar-nav-links"}>
 					<ul className = "NavBar-nav-list">
-						<li><Link to  = {"/play/" + this.props.gameID} className = "NavBar-link" onClick = {this.props.setNewGameID}>New Game</Link></li>
-						<li><Link to = "/play/" className = "NavBar-link">Join Game</Link></li>
-            <li><Link to  = {"/create"} className = "NavBar-link">Create Deck</Link></li>
-            {this.props.userId?<li><Link to = {`/profile/${this.props.userId}`} className = "NavBar-link">Profile</Link></li>:null}
-            <li>{this.props.userId ? (
+						<li className = "NavBar-nav-item"><Link to  = {"/play/" + this.props.gameID} className = "NavBar-link" onClick = {this.props.setNewGameID}>New Game</Link></li>
+						<li className = "NavBar-nav-item"><Link to = "/play/" className = "NavBar-link">Join Game</Link></li>
+            <li className = "NavBar-nav-item"><Link to  = {"/create"} className = "NavBar-link">Create Deck</Link></li>
+            {this.props.userId?<li className = "NavBar-nav-item"><Link to = {`/profile/${this.props.userId}`} className = "NavBar-link">Profile</Link></li>:null}
+            <li className = "NavBar-nav-item">{this.props.userId ? (
               <GoogleLogout
                 clientId={GOOGLE_CLIENT_ID}
                 buttonText="Logout"
