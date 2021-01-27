a3c0a25458ef55b3d2d0217a1663a765c9b95a07
Kevin Tong <kevintong@C02CJ5N6MD6M.tld>
2021-01-26 21:56:59 -0800

    Added remaining rank icons

---
+++ client/src/components/modules/icons/ninthplace.js
@@ -0,0 +1,23 @@
+/*<!-- Generator: Adobe Illustrator 25.1.0, SVG Export Plug-In  -->*/
+import React from 'react';
+import './rankicon.css';
+
+const NinthPlace = (props) => {
+  return(
+    <div>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="706px"
+        height="1115px" viewBox="0 0 706 1115" className = "RankIcon-styling">
+      <defs>
+      </defs>
+      <g>
+        <path d="M353,0C158,0,0,142.4,0,318c0,172.4,152.3,312.8,342.3,317.9L84.8,1082c-8.5,14.7,2.1,33,19.1,33h167.1
+          c11.8,0,22.8-6.3,28.7-16.6l356.3-617.2c31.8-47.7,50-103.6,50-163.3C706,142.4,547.9,0,353,0z M353,480.4
+          c-99.4,0-180-72.6-180-162.1c0-89.6,80.6-162.2,180-162.2c99.4,0,180,72.6,180,162.2c0,19.9-4,39-11.3,56.7
+          c-14.4,34.7-41.5,63.7-76.3,82.5C418.3,472,386.7,480.4,353,480.4z"/>
+      </g>
+      </svg>
+    </div>
+  )
+}
+
+export default NinthPlace;
---
+++ client/src/components/modules/icons/seventhplace.js
@@ -0,0 +1,20 @@
+/*<!-- Generator: Adobe Illustrator 25.1.0, SVG Export Plug-In  -->*/
+import React from 'react';
+import './rankicon.css';
+
+const SeventhPlace = (props) => {
+  return(
+    <div>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="801.7px"
+        height="1062px" viewBox="0 0 801.7 1062" className = "RankIcon-styling">
+      <defs>
+      </defs>
+      <g>
+        <polygon points="801.7,144.2 271.8,1062 132.4,981.5 615.4,145 0,145 0,0 801.2,0 801.2,143.9 	"/>
+      </g>
+      </svg>
+    </div>
+  )
+}
+
+export default SeventhPlace;
